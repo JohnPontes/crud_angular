8f3f28d0089c7984dc73e2c0aa8238f37a69c2e4
John Wesley <jwesleysilvap@gmail.com>
2021-05-12 12:14:49 -0300

    terceiro commit

--- src/app/components/product/product-create/product-create.component.ts
+++ src/app/components/product/product-create/product-create.component.ts
@@ -1,4 +1,7 @@
+import { Product } from './../product.model';
+import { ProductService } from '../product.service';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-product-create',
@@ -6,15 +9,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-create.component.css']
 })
 export class ProductCreateComponent implements OnInit {
-    propLegal = "qualquer"
 
-  constructor() { }
+  product: Product = {
+    name: '',
+    price: null as any
+  }
+
+  constructor(private productService: ProductService,
+     private router: Router) { }
 
   ngOnInit(): void {
-  }
-  
-  fazerAlgo(): void {
-    console.log('Fazendo algo!')
+    
   }
 
+  createProduct(): void {
+    this.productService.create(this.product).subscribe(() => {
+    this.productService.showMessage('Operação executada com sucesso!')
+    this.router.navigate(['/products'])
+
+    })
+  }
+  cancel(): void {
+    this.router.navigate(['/products'])
+  }
 }
